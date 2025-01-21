5b2bd086678bfa051656256fade0f08aa154a449
Bojan <bojan.niki@gmail.com>
2025-01-21 20:09:34 +0100

    Added the basic html structure and styling

---
+++ javascript.js
@@ -0,0 +1,9 @@
+// Function to create
+function showInstructions() {
+  var instructions = document.getElementById("instructions");
+  if (instructions.style.display === "none") {
+    instructions.style.display = "block";
+  } else {
+    instructions.style.display = "none";
+  }
+}
