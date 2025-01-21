278cefbfaacca5828ca18d6b9b3c49a26b1d91e0
Bojan <bojan.niki@gmail.com>
2025-01-21 20:54:45 +0100

    Started to add the functionality

--- javascript.js
+++ javascript.js
@@ -7,3 +7,29 @@ function showInstructions() {
     instructions.style.display = "none";
   }
 }
+let isPaused = false;
+let isStopped = false;
+let displayElement = document.getElementById("display");
+function fizzBuzz() {
+  let startNumber = parseFloat(document.getElementById("start-input").value);
+  let stopNumber = parseFloat(document.getElementById("stop-input").value);
+  let fizz = document.getElementById("fizz-input").value;
+  let buzz = document.getElementById("Buzz-input").value;
+  let fizzBuzz = document.getElementById("fizzBuzz-input").value;
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms)); //created the delay
+  displayElement.textContent = ""; //Clearing the display at the start
+
+  const stopButton = document.getElementById("stopButton"); //created the stop button
+  stopButton.addEventListener("click", () => {
+    isStopped = true; // Set the flag to true when the button is clicked
+    displayElement.textContent = "Loop stopped";
+  });
+}
+if (
+  !Number.isInteger(startNumber) ||
+  !Number.isInteger(stopNumber) ||
+  startNumber <= 0 ||
+  stopNumber <= 0
+) {
+  displayElement.textContent = "Please input a positive integer";
+}
