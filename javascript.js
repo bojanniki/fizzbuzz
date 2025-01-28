6c4a286839b4dba2075b29bfa01481d064f734a3
Bojan <bojan.niki@gmail.com>
2025-01-28 20:49:23 +0100

    Added the stop and reset buttons functionality

--- javascript.js
+++ javascript.js
@@ -10,6 +10,10 @@ function showInstructions() {
 
 let displayElement = document.getElementById("display");
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+document.getElementById("stopButton").disabled = true; //locked buttons before the start of the function
+document.getElementById("pauseButton").disabled = true;
+document.getElementById("resumeButton").disabled = true;
+let isStopped = false;
 
 async function fizzBuzz() {
   document.getElementById("startInput").disabled = true; //locked the inputs at the start of the function
@@ -17,8 +21,13 @@ async function fizzBuzz() {
   document.getElementById("fizzInput").disabled = true;
   document.getElementById("buzzInput").disabled = true;
   document.getElementById("fizzBuzzInput").disabled = true;
-  document.getElementById("fizzNumber").disabled = true;
+  document.getElementById("fizzNumber").disabled = true; //enabling/disabling buttons before the start of the function
   document.getElementById("buzzNumber").disabled = true;
+  document.getElementById("startButton").disabled = true;
+  document.getElementById("resetButton").disabled = true;
+  document.getElementById("stopButton").disabled = false;
+  document.getElementById("pauseButton").disabled = false;
+
   let startNumber = parseFloat(document.getElementById("startInput").value);
   let stopNumber = parseFloat(document.getElementById("stopInput").value);
   let fizzNumber = parseFloat(document.getElementById("fizzNumber").value);
@@ -26,6 +35,7 @@ async function fizzBuzz() {
   let fizz = document.getElementById("fizzInput").value || "Fizz"; // fizz is default
   let buzz = document.getElementById("buzzInput").value || "Buzz"; // Buzz is default
   let fizzBuzz = document.getElementById("fizzBuzzInput").value || "FizzBuzz"; // fizzBuzz is default
+
   // input validations
   if (isNaN(startNumber) || isNaN(stopNumber)) {
     displayElement.textContent = "Please enter valid numbers for the range.";
@@ -44,7 +54,9 @@ async function fizzBuzz() {
   // fizzBuzz logic
   for (let i = startNumber; i <= stopNumber; i++) {
     // input values
-
+    if (isStopped == true) {
+      break;
+    }
     let result = "";
     if (i % fizzNumber === 0 && i % buzzNumber === 0) {
       result = fizzBuzz;
@@ -61,3 +73,35 @@ async function fizzBuzz() {
     await delay(1000); // Add a delay of 1000 ms
   }
 }
+const stopButton = document.getElementById("stopButton"); // stop button functionality
+stopButton.addEventListener("click", function () {
+  isStopped = true;
+  displayElement.textContent = "Stop button pressed";
+  document.getElementById("startButton").disabled = true;
+  document.getElementById("stopButton").disabled = true;
+  document.getElementById("resumeButton").disabled = true;
+  document.getElementById("pauseButton").disabled = true;
+  document.getElementById("resetButton").disabled = false;
+});
+const resetButton = document.getElementById("resetButton"); // reset button functionality
+resetButton.addEventListener("click", async function () {
+  displayElement.textContent = "Reset button pressed";
+  await delay(1000);
+  displayElement.textContent = "";
+  document.getElementById("startButton").disabled = false; //Reset input and button conditions
+  document.getElementById("resetButton").disabled = false;
+  document.getElementById("startInput").disabled = false;
+  document.getElementById("stopInput").disabled = false;
+  document.getElementById("fizzInput").disabled = false;
+  document.getElementById("buzzInput").disabled = false;
+  document.getElementById("fizzBuzzInput").disabled = false;
+  document.getElementById("fizzNumber").disabled = false;
+  document.getElementById("buzzNumber").disabled = false;
+  document.getElementById("startInput").value = "";
+  document.getElementById("stopInput").value = "";
+  document.getElementById("fizzNumber").value = "";
+  document.getElementById("buzzNumber").value = "";
+  document.getElementById("fizzInput").value = "";
+  document.getElementById("buzzInput").value = "";
+  document.getElementById("fizzBuzzInput").value = "";
+});
